2a223399b5c97f913acad13e137d8909f0d5221b
Hemant Vishwakarma <hemantx96@gmail.com>
2021-05-18 13:52:26 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     poweredByHeader: false,
     env: {
-      KEY   : 'AIzaSyAWlxn63an0GmZsskzDO3lWLH1Kn85v3aM',
-      CX    : '02180fbcd815db156',
+      KEY   : 'YOUR KEY',
+      CX    : 'YOUR CX',
      
     }    
   }
