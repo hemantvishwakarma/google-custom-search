5687e228ee54d0560a6d1b653aca446500bb2052
Hemant Vishwakarma <hemantx96@gmail.com>
2021-05-18 13:51:40 +0530

    just now

--- pages/api/googlesearch.js
+++
@@ -1,9 +0,0 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
-export default (req, res) => {
-
-  fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.KEY}&cx=${process.env.CX}&q=amazon trending products`)
-  .then(response => response.json())
-  .then(data => res.status(200).json(data))
-  .catch(err => res.status(500).send('Server Error',err));
-}
