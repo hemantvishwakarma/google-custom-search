18a1b6a4b3afb849c739bd2d7474990eab4aeb35
Hemant Vishwakarma <hemantx96@gmail.com>
2021-05-20 16:27:25 +0530

    20

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 module.exports = {
     poweredByHeader: false,
     env: {
-      KEY   : process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
-      CX    : process.env.NEXT_PUBLIC_GOOGLE_CX,
+      NEXT_PUBLIC_GOOGLE_API_KEY   : process.env.NEXT_PUBLIC_GOOGLE_API_KEY,
+      NEXT_PUBLIC_GOOGLE_API_KEY    : process.env.NEXT_PUBLIC_GOOGLE_CX,
      
     }    
   }
