f3524f6b4bff05579c9d67b55e45ee049011f4d5
Hemant Vishwakarma <hemantx96@gmail.com>
2021-05-21 23:00:14 +0530

    21-05-2021

--- pages/index.js
+++ pages/index.js
@@ -1,87 +1,41 @@
-import react, {useState} from 'react';
+import react, {useEffect, useState} from 'react';
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
 
-  const [allData, setAllData] = useState([]);
   const [data, setData] = useState([]);
-  const [loader, setLoader] = useState(false);
 
 
-  const fetchData = async (start=1, q) => { 
-    console.log(start)   
-    return fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&cx=${process.env.NEXT_PUBLIC_GOOGLE_CX}&q=${q}&start=${start}`)
+  const fetchData = (start=1, q) => {
+    
+    fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.NEXT_PUBLIC_GOOGLE_API_KEY}&cx=${process.env.NEXT_PUBLIC_GOOGLE_CX}&q=${q}&start=${start}`)
     .then(response => response.json())
     .then(data => {
-      setAllData(data)
-      // console.log(data)
-      return data.items;
+      // console.log(data);
+      setData(data)
     })
     .catch(err => res.status(500).send('Server Error',err));
   }
 
   const onSubmit = async (e) => {
-    setLoader(true);
-    setData([])
     const formData = new FormData(e.target)
-    const Fdata = {}
+    const data = {}
     e.preventDefault()
     for (let entry of formData.entries()) {
-      Fdata[entry[0]] = entry[1]
+        data[entry[0]] = entry[1]
     } 
 
-    var i;
-    let jsonData = {};
-    for (i = 1; i <= 30; i++) {      
-     await fetchData(i, e.target.query.value).then(dd => {  
-      //  console.log(dd)      
-        jsonData[i] = dd     
-      });
-    }
-    setData(jsonData);
-    setLoader(false);
+    fetchData(1, e.target.query.value)
   }
 
-  const listData = (data) => {  
-    console.log(data);
-    var i;
-    let looplistData='';
-    const looplistArrayData = [];
-
-    if(data.length!=0){
-      for (i = 1; i <=30; i++) {      
-        {data[i].map((itemData, index) => {
-          looplistArrayData.push(itemData.link);
-          // looplistData+=`<li key=${index} className="list-group-item"> - ${itemData.link}</li>`;
-        }                    
-      )} 
-      }
-    }   
-
-    // 
-    let uniqueLoopData = [...new Set(looplistArrayData)];
-    {looplistArrayData.map((itmData, index) => {
-      looplistData+=`<li key=${index} className="list-group-item"> - ${itmData}</li>`;
-    }  )}
-
-
-    return looplistData;
-  }
-
-  // react.useEffect(() => {
-  //   let chars = ['A', 'B', 'A', 'C', 'B'];
-  //   let uniqueChars = [...new Set(chars)];
-    
-  //   console.log(uniqueChars);
-  // }, [])
 
   return (
     <div className={styles.container}>
       <Head>
         <title>Search Engine</title>
       </Head>   
-      
+
       <div className="container">
         <form onSubmit={ (e) => onSubmit(e)}>
           <div className="col-lg-12 text-center mt-5">
@@ -97,32 +51,30 @@ export default function Home() {
           </div>
         </form>  
 
-        {loader ? 
-        <>
-        <p className="loader text-left">Please wait... ( 30-45 Seconds)</p>          
-        </>
-      : '' }
+        {data.items ? 
+          <>
+          <ul className="list-group mt-5">
+            {(data.items) && data.items.map((itemData, index) => (
+              <li key={index} className="list-group-item">{itemData.link}</li>            
+            ))}
+          </ul>
 
-        {data ? 
-          <>       
-
-          {data ? 
-          <ul className="list-group mt-5 mb-15" dangerouslySetInnerHTML={{__html: listData(data)}}></ul>
-          : ''}    
-          
-{/* 
-          {data ? 
           <nav aria-label="Page navigation example">
 
             <ul className="pagination mt-5">
-              {allData.queries.previousPage ?
-              <li className="page-item"><a className="page-link" onClick={() => fetchData(allData.queries.previousPage[0].startIndex, allData.queries.previousPage[0].searchTerms)}>Previous Page</a></li>
-              : ''}             
-              <li className="page-item"><a className="page-link" onClick={() => fetchData(allData.queries.nextPage[0].startIndex, allData.queries.nextPage[0].searchTerms)}>Next Page</a></li>
+              {data.queries.previousPage ?
+              <li className="page-item"><a className="page-link" onClick={() => fetchData(data.queries.previousPage[0].startIndex, data.queries.previousPage[0].searchTerms)}>Previous Page</a></li>
+              : ''}
+
+              {/* <li className="page-item"><a className="page-link" href="#">1</a></li>
+              <li className="page-item"><a className="page-link" href="#">2</a></li>
+              <li className="page-item"><a className="page-link" href="#">3</a></li> */}
+               {data.queries.nextPage ?
+              <li className="page-item"><a className="page-link" onClick={() => fetchData(data.queries.nextPage[0].startIndex, data.queries.nextPage[0].searchTerms)}>Next Page</a></li>
+              : ''}
             </ul>
 
           </nav>
-           : ''}    */}
           </>
         : ''}
 
